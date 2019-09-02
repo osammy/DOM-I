14642f7f51356ec18b4a714493545a85415491f1
samuel <osamaimafidon@gmail.com>
2019-09-02 14:51:33 +0100

    Finished stretch project

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,36 +1,39 @@
-// const digits = document.querySelector(".digits");
+const digits = document.querySelector(".digits");
 
-// const secondOnes = document.getElementById("secondOnes");
-// const secondTens = document.getElementById("secondTens");
-// const hundreds = document.getElementById("msHundreds");
-// const tens = document.getElementById("msTens");
+function init() {
+  const secondOnes = document.getElementById("secondOnes");
+  const secondTens = document.getElementById("secondTens");
+  const hundreds = document.getElementById("msHundreds");
+  const tens = document.getElementById("msTens");
+  hundreds.textContent = 0;
+  tens.textContent = 0;
+  secondOnes.textContent = 0;
+  secondTens.textContent = 0;
+}
 
-// function init() {
-//   hundreds.textContent = 0;
-//   tens.textContent = 0;
-//   secondOnes.textContent = 0;
-//   secondTens.textContent = 0;
-// }
+function startCounting() {
+  const secondOnes = document.getElementById("secondOnes");
+  const secondTens = document.getElementById("secondTens");
+  const hundreds = document.getElementById("msHundreds");
+  const tens = document.getElementById("msTens");
+  const timerInterval = setInterval(() => {
+    tens.textContent = Number(tens.textContent) + 1;
+    if (Number(tens.textContent) === 10) {
+      tens.textContent = 0;
+      hundreds.textContent = Number(hundreds.textContent) + 1;
+      if (Number(hundreds.textContent) === 10) {
+        hundreds.textContent = 0;
+        secondOnes.textContent = Number(secondOnes.textContent) + 1;
+        if (Number(secondOnes.textContent) === 10) {
+          secondTens.textContent = 1;
+          secondOnes.textContent = 0;
+          digits.style.color = "red";
+          clearInterval(timerInterval);
+        }
+      }
+    }
+  }, 10);
+}
 
-// function startCounting() {
-// //   const hundreds = document.getElementById("#msHundreds");
-// //   const tens = document.getElementById("#msTens");
-//     let count = 0;
-//   const timerInterval = setInterval(() => {
-//     hundreds.textContent = Number(hundreds.textContent) + 1;
-//     // console.log(typeof hundreds.textContent);
-//     if (Number(hundreds.textContent) === 6) {
-//       hundreds.textContent = 0;
-//       secondOnes.textContent = Number(secondOnes.textContent) + 1;
-
-//       if (Number(secondOnes.textContent) === 9) {
-//         secondTens.textContent = 1;
-//         secondOnes.textContent = 0;
-//         clearInterval(timerInterval);
-//       }
-//     }
-//   }, 10);
-// }
-
-// init();
-// startCounting();
+init();
+startCounting();
